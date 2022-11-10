bd7c92194be79e5ee53cae96cd5ec53e857a8b3e
Luis Santos <luis.santos@nfsconsultoria.com.br>
2022-11-10 08:41:26 -0300

    Arquetipo

---
+++ src/main/kotlin/br/com/vr/enderecoapi/EnderecoApiApplication.kt
@@ -0,0 +1,11 @@
+package br.com.vr.enderecoapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EnderecoApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<EnderecoApiApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/vr/enderecoapi/EnderecoApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.vr.enderecoapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EnderecoApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
